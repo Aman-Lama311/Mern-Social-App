f2f86e20801e04676312e4140df103ca96dadfbd
Aman-Lama311 <lamaaman2036@gmail.com>
2025-06-17 15:53:00 +0545

    pushed last time maybe

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -95,6 +95,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         sameSite: "none",
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000,
       })
       .json({
