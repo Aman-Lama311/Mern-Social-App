82b102e689de46e084b265df9572a4bf93e3ca38
Aman-Lama311 <lamaaman2036@gmail.com>
2025-06-17 16:24:33 +0545

    all error fixed

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -33,7 +33,9 @@ const Post = ({ post }) => {
     try {
       const action = liked ? "dislike" : "like";
       const res = await axios.get(
-        `http://localhost:8000/api/v1/post/${post?._id}/${action}`,
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/post/${
+          post?._id
+        }/${action}`,
         { withCredentials: true }
       );
       if (res.data.success) {
@@ -63,7 +65,7 @@ const Post = ({ post }) => {
   const commentHandler = async () => {
     try {
       const res = await axios.post(
-        `http://localhost:8000/api/v1/post/${post?._id}/comment`,
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/post/${post?._id}/comment`,
         { text },
         {
           headers: {
@@ -92,7 +94,7 @@ const Post = ({ post }) => {
   const deletePostHandler = async () => {
     try {
       const res = await axios.delete(
-        `http://localhost:8000/api/v1/post/delete/${post?._id}`,
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/post/delete/${post?._id}`,
         { withCredentials: true }
       );
       if (res.data.success) {
